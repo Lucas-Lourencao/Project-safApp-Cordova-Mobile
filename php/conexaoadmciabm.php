9d1a2c505cf92de2f8886737dcb5f4ae7fb3e9cb
Lucas Lourenção <lucas.l.lourencao@gmail.com>
2023-03-07 04:56:56 -0300

    "Reconstruindo o projeto safApp"

--- php/conexaoadmciabm.php
+++
@@ -1,5 +0,0 @@
-<?php
-$conexao = mysqli_connect('localhost', 'inte0174_cmtcia', '1234560654321');
-$banco = mysqli_select_db($conexao,'inte0174_admciabm');
-mysqli_set_charset($conexao,'utf8');
-?>
